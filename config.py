ba5a4cac364503424aa905b95d8550faf96bc3fe
botbsam <bsamalqhwm@gmail.com>
2024-07-27 12:21:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/botzon/music",
+    "https://github.com/botbsam/music",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
