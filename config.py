3134f5fe986b2c98892b2cfdb695ea7060767e2a
botlole <njm88507@gmail.com>
2024-10-08 23:21:44 +0300

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/bov2/fmusic",
+    "https://github.com/botlole/music",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
